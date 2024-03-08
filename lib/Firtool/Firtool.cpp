4cb7dbe44e57acb524a00584912a2f09a69b4a00
Dobios <adobis@student.ethz.ch>
2024-03-08 12:07:25 -0800

    firtool integration

--- lib/Firtool/Firtool.cpp
+++ lib/Firtool/Firtool.cpp
@@ -344,8 +344,8 @@ populatePrepareForExportVerilog(mlir::PassManager &pm,
 LogicalResult firtool::populateHWToBTOR2(mlir::PassManager &pm,
                                          const FirtoolOptions &opt,
                                          llvm::raw_ostream &os) {
-  pm.addPass(circt::createLowerLTLToCorePass());
-  pm.addPass(circt::createConvertHWToBTOR2Pass(os));
+  pm.addNestedPass<hw::HWModuleOp>(circt::createLowerLTLToCorePass());
+  pm.addNestedPass<hw::HWModuleOp>(circt::createConvertHWToBTOR2Pass(os));
   return success();
 }
 
